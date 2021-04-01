fa9b521b427314088cec9da3ceed70e11daa6de3
Tyy1128 <80470290+Tyy1128@users.noreply.github.com>
2021-04-01 18:10:44 +0800

    第四次Commit

--- src/软件工程/Print.java
+++ src/软件工程/Print.java
@@ -1,14 +1,17 @@
 package Èí¼þ¹¤³Ì;
 
+import java.io.IOException;
 import java.util.Map;
 
-public class Print {
-
-	public Print(String string) {
-		// TODO Auto-generated constructor stub
+public class Print 
+{
+	public Print(String file) 
+	{
+		String outputfile = file;
 	}
 
-	public void SortMap(Map<String, Integer> wordsCount, int wordline, int wordcount, int i) {
+	public void SortMap(Map<String, Integer> oldmap, int wordline, int wordcount, int characterscount) throws IOException
+	{
 		// TODO Auto-generated method stub
 		
 	}
