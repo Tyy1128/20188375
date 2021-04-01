790cfd1803ceb87da51dc457194f188ee6d6ae53
Tyy1128 <80470290+Tyy1128@users.noreply.github.com>
2021-04-01 14:04:11 +0800

    第二次Commit

--- src/软件工程/Test.java
+++ src/软件工程/Test.java
@@ -6,20 +6,47 @@ import java.util.Map;
 
 public class Test 
 {
-  public static void CountEachLineCharacter(String line) 
+  public static int CountEachLineCharacter(String line) 
   {
-     
+      return line.length();
   }
 
-  public static void CountWordNum(String line, List<String> lists) 
+  public static int CountWordNum(String line, List<String> lists) 
   {
-      
-    
+      int wordcount = 0;
+      String[] wordsArr1 = line.split("[^a-zA-Z0-9]");  //¹ýÂË
+
+      for (String newword : wordsArr1) 
+      {  
+          if(newword.length()!= 0) 
+          {               	
+              if((newword.length() >= 4) && (Character.isLetter(newword.charAt(0))
+                  && Character.isLetter(newword.charAt(1)) && Character.isLetter(newword.charAt(2))
+                  && Character.isLetter(newword.charAt(3)))) 
+              {               		
+                  wordcount++;
+                  lists.add(newword);  
+              }
+          }  
+      }
+
+      return wordcount;
   }
 
   public static void CountEachWordNum(Map<String, Integer> wordsCount,List<String> lists) 
   {     
-      
+      for (String li : lists) 
+      {  
+          if(wordsCount.get(li) != null) 
+          { 
+              wordsCount.put(li,wordsCount.get(li) + 1);  
+          }
+          else 
+          {  
+              wordsCount.put(li,1);  
+          }  
+
+      }  
   }
 
 }
