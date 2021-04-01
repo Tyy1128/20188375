076d1c67598b1810a6de044e36126eb5197fa56f
Tyy1128 <80470290+Tyy1128@users.noreply.github.com>
2021-04-01 18:34:51 +0800

    第五次Commit

--- src/软件工程/Print.java
+++ src/软件工程/Print.java
@@ -1,19 +1,55 @@
 package Èí¼þ¹¤³Ì;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Print 
 {
-	public Print(String file) 
-	{
-		String outputfile = file;
-	}
+	String outputfile ;
+    Print(String file) 
+    {
+    	outputfile = file;
+    }
 
-	public void SortMap(Map<String, Integer> oldmap, int wordline, int wordcount, int characterscount) throws IOException
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
+    public void SortMap(Map<String,Integer> oldmap,int wordline,int wordcount,int characterscount) throws IOException 
+    {  
+          
+        ArrayList<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(oldmap.entrySet());  
+          
+        Collections.sort(list,new Comparator<Map.Entry<String,Integer>>() 
+        {  
+            public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) 
+            {  
+                return o2.getValue() - o1.getValue();   
+            }  
+        }); 
+        File file = new File(outputfile);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+        bw.write("characters: "+characterscount+"\n");
+        bw.write("words: "+wordcount+"\n");
+        bw.write("lines: "+wordline+"\n");
+        
+        int flag = 0;
+        
+        for(int i = 0; i<list.size(); i++) 
+        {  
+        	if(flag>=10) 
+        	{
+                break;
+            } 
+        	if(list.get(i).getKey().length()>=4) 
+        	{
+        		bw.write(list.get(i).getKey()+ ": " +list.get(i).getValue()+"\n"); 
+            }
+        	flag++;
+        }
+        bw.close();
+    }  
 }
